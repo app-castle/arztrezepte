3c2e6bf3e4a89e066f6f1db585e73b4771f33118
admir86 <admir.ramic@outlook.com>
2022-08-27 00:13:40 +0200

    style: format main.ts

--- workspace/apps/arztrezepte/src/main.ts
+++ workspace/apps/arztrezepte/src/main.ts
@@ -2,8 +2,8 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { environment } from './environments/environment';
 
@@ -37,7 +37,8 @@ bootstrapApplication(AppComponent, {
             ),
         },
       ]),
-      provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore())
     ),
   ],
 }).catch((err) => console.error(err));
