b3dbe66973f14004d67e8a2ba5b4f51772fb568a
Angie Haider <nefertiri1402@freenet.de>
2022-08-26 23:38:58 +0200

    feat: firebase added

--- workspace/apps/arztrezepte/src/app/app.module.ts
+++ workspace/apps/arztrezepte/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
