791a9f86dbecd54c946ae9c74a9c6d5582ac2eed
admir86 <admir.ramic@outlook.com>
2022-08-27 20:38:51 +0200

    fix: reload pharmacy reload

--- workspace/apps/arztrezepte/src/app/pharmacy/pharmacy.component.ts
+++ workspace/apps/arztrezepte/src/app/pharmacy/pharmacy.component.ts
@@ -1,22 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatAutocompleteModule,
-  MatAutocompleteSelectedEvent,
-} from '@angular/material/autocomplete';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
-import { Recipe } from '../shared/recipe.models';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute } from '@angular/router';
 import { Medication } from '../shared/medication.models';
 import { Patient } from '../shared/patient.models';
+import { Recipe } from '../shared/recipe.models';
 import { RecipeService } from '../shared/recipe.service';
+
+type RecipeModel = Recipe & { medication: Medication; patient: Patient };
 
 @Component({
   selector: 'bh-pharmacy',
@@ -37,24 +34,19 @@ import { RecipeService } from '../shared/recipe.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PharmacyComponent {
-  
-  public recipe$: Observable<Recipe & { medication: Medication, patient: Patient }>;
+  public recipe: RecipeModel;
 
   constructor(
-    route: ActivatedRoute,
-    public recipeService: RecipeService)
-  {
+    private route: ActivatedRoute,
+    public recipeService: RecipeService
+  ) {
     //this.recipe$ = this.recipeService.getCompleteRecipe("uEtJdGasXsim9ByzvL5T");
 
-    this.recipe$ = route.data.pipe(
-      map((data) => data['recipe']),
-      filter((recipe) => !!recipe)
-    );
+    this.recipe = this.route.snapshot.data['recipe'];
   }
 
-  public async onSave(recipe: Recipe & { medication: Medication, patient: Patient }): Promise<void> {
-    recipe.Used=true;
-    await this.recipeService.update(recipe.id, recipe);
+  public async onSave(): Promise<void> {
+    this.recipe.Used = true;
+    await this.recipeService.update(this.recipe.id, { ...this.recipe });
   }
-
 }
