7e3be5d3878d575e63419c8875cbe8f7b037d363
admir86 <admir.ramic@outlook.com>
2022-08-27 16:00:33 +0200

    feat(doctor): medication selector

--- workspace/apps/arztrezepte/src/app/doctor/doctor.component.ts
+++ workspace/apps/arztrezepte/src/app/doctor/doctor.component.ts
@@ -10,8 +10,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { Observable } from 'rxjs';
 import { map, startWith, switchMap } from 'rxjs/operators';
+import { Medication } from '../shared/medication.models';
+import { MedicationService } from '../shared/medication.service';
 import { Patient } from '../shared/patient.models';
 import { PatientService } from '../shared/patient.service';
+import { Recipe } from '../shared/recipe.models';
 
 @Component({
   selector: 'bh-doctor',
@@ -29,14 +32,24 @@ import { PatientService } from '../shared/patient.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DoctorComponent {
+  public recipe: Recipe = {} as any;
+
   public patients$: Observable<Patient[]>;
   public filteredPatients$: Observable<Patient[]>;
   public patientInput = new FormControl('');
   public selectedPatient: Patient | null = null;
 
-  constructor(public service: PatientService) {
-    this.patients$ = this.service.getAll();
-    this.filteredPatients$ = this.initAutocomplete();
+  public medications$: Observable<Medication[]>;
+  public filteredMedications$: Observable<Medication[]>;
+  public medicationInput = new FormControl('');
+  public selectedMedication: Medication | null = null;
+
+  constructor(patientService: PatientService, medicationService: MedicationService) {
+    this.patients$ = patientService.getAll();
+    this.filteredPatients$ = this.initPatientsAutocomplete();
+
+    this.medications$ = medicationService.getAll();
+    this.filteredMedications$ = this.initMedicationsAutocomplete();
   }
 
   public onPatientSelected(args: MatAutocompleteSelectedEvent) {
@@ -47,7 +60,7 @@ export class DoctorComponent {
     return p ? `${p.Firstname} ${p.Lastname}` : '';
   }
 
-  private initAutocomplete() {
+  private initPatientsAutocomplete() {
     return this.patientInput.valueChanges.pipe(
       startWith(''),
       switchMap((value) => {
@@ -65,6 +78,36 @@ export class DoctorComponent {
           (p) =>
             p.Firstname.toLowerCase().includes(filterValue) ||
             p.Lastname.toLowerCase().includes(filterValue)
+        )
+      )
+    );
+  }
+
+  public onMedicationSelected(args: MatAutocompleteSelectedEvent) {
+    this.selectedMedication = args?.option?.value;
+  }
+  public medicationDisplayFn(p: Medication): string {
+    return p ? `${p.Name}` : '';
+  }
+
+  private initMedicationsAutocomplete() {
+    return this.medicationInput.valueChanges.pipe(
+      startWith(''),
+      switchMap((value) => {
+        const name = typeof value === 'string' ? value : null;
+        return name ? this.filterMedications(name) : this.medications$;
+      })
+    );
+  }
+  private filterMedications(value: string): Observable<Medication[]> {
+    const filterValue = value.toLowerCase();
+
+    return this.medications$.pipe(
+      map((p) =>
+        p.filter(
+          (p) =>
+            p.Name.toLowerCase().includes(filterValue) ||
+            p.Company.toLowerCase().includes(filterValue)
         )
       )
     );
