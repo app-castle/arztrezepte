033650690c56a2867cadb5b5fe119f6f2a91bbfe
Markus Chiarot <cmarkus@hotmail.ch>
2022-08-27 20:36:33 +0200

    feat: show some more recipe details

--- workspace/apps/arztrezepte/src/app/patient/patient-recipe-detail/patient-recipe-detail.component.ts
+++ workspace/apps/arztrezepte/src/app/patient/patient-recipe-detail/patient-recipe-detail.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Recipe } from '../../shared/recipe.models';
-import { Patient } from '../../shared/patient.models';
-import { Medication } from '../../shared/medication.models';
-import { map, filter } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
 import { QRCodeModule } from 'angularx-qrcode';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { Medication } from '../../shared/medication.models';
+import { Patient } from '../../shared/patient.models';
+import { Recipe } from '../../shared/recipe.models';
 import { PatientToolbarService } from '../patient-toolbar.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'bh-patient-recipe-detail',
   standalone: true,
-  imports: [CommonModule, QRCodeModule],
+  imports: [
+    CommonModule,
+    QRCodeModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './patient-recipe-detail.component.html',
   styleUrls: ['./patient-recipe-detail.component.scss'],
 })
@@ -35,5 +48,11 @@ export class PatientRecipeDetailComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.toolbarService.setBacklinkState(false);
+  }
+
+  isValid(date: string) {
+    const today = new Date();
+    const validTo = new Date(date);
+    return validTo > today;
   }
 }
